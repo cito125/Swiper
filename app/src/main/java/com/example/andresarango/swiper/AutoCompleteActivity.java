26b89fc5b5e3e36426814279a3f1f2bb7aad957c
cito125 <cito125@gmail.com>
2017-02-10 23:49:23 -0500

    added autocomplete fragment

--- app/src/main/java/com/example/andresarango/swiper/AutoCompleteActivity.java
+++ app/src/main/java/com/example/andresarango/swiper/AutoCompleteActivity.java
@@ -3,11 +3,37 @@ package com.example.andresarango.swiper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
 public class AutoCompleteActivity extends AppCompatActivity {
+
+    private PlaceAutocompleteFragment mAutocompleteFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto_complete);
+        mAutocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                .build();
+
+        mAutocompleteFragment.setFilter(typeFilter);
+        mAutocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                System.out.println(place.getId());;
+            }
+
+            @Override
+            public void onError(Status status) {
+
+            }
+        });
     }
 }
